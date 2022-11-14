7d91acea5d30bbf0231030675190f10a8990e56e
robs-am <roberta.amaro89@gmail.com>
2022-11-13 23:28:17 -0300

    add Origamid folder

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+
+
+var nome = "Roberta";
+var idade = 33;
+var comidaFavorita;
+var comidaFavorita = "Moqueca";
+
+var carroDeCorrida;
+    numeroDaSorte;
+    diaDaSemana;
+    pokemonFavorito;
+    RadioButtonUnchecked;
