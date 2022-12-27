0006a6697b70f0e95724feb68505fa40ac133bd2
robs-am <roberta.amaro89@gmail.com>
2022-12-27 19:46:57 -0300

    add switch condicional

---
+++ boolean-e-condicionais/script.js
@@ -0,0 +1,18 @@
+var corFavorita = 'Amarelo';
+
+switch (corFavorita) {
+  case 'Azul':
+    console.log('Olhe para o céu.');
+    break;
+  case 'Vermelho':
+    console.log('Olhe para rosas.');
+    break;
+  case 'Amarelo':
+    console.log('Olhe para o sol.');
+    break;
+  default:
+    console.log('Feche os olhos');
+}
+
+
+
