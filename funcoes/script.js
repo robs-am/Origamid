51529cc6e4365729d2c0a21b0203e46b8b29ed70
robs-am <roberta.amaro89@gmail.com>
2023-01-03 23:13:25 -0300

    add continuação funcoes

--- funcoes/script.js
+++ funcoes/script.js
@@ -1,5 +1,38 @@
 function areaQuadrado(lado) {
-  console.log('funçao ativa')
+  return lado * lado;
 }
 
-areaQuadrado();
+console.log(areaQuadrado(10));
+
+//Parametro x Argumento
+//Parametro é quando vc cria a função
+//Argumeto são os valores que substitutem os parametros ao executá-la
+
+function imc(peso, altura) {
+  var imc = peso / (altura * altura)
+  return imc
+}
+
+console.log(imc(64,1.7))
+
+function corFavorita(cor) {
+  if(cor === "azul") {
+    return "Eu adoro o céu"
+  } else if( cor === 'verde'){
+    return "Eu gosto da natureza"
+  }else{
+    return "Eu não gosto de cores"
+  }
+}
+
+
+
+
+addEventListener('click', function() {
+  console.log('Clicou');
+});
+
+// A função possui dois argumentos
+// Primeiro é a string 'click' que significa quando o usuário clica em algo
+// Segundo é uma função anônima que é executada somente quando o primeiro argumento 'click' ocorre
+
