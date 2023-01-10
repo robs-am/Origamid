488f6b9af4d2a63c2b30e91b35c6c7740de48df9
Roberta Amaro <roberta.amaro@ed3.com.br>
2023-01-09 22:42:18 -0300

    add addEventListener

--- funcoes/script.js
+++ funcoes/script.js
@@ -9,30 +9,26 @@ console.log(areaQuadrado(10));
 //Argumeto são os valores que substitutem os parametros ao executá-la
 
 function imc(peso, altura) {
-  var imc = peso / (altura * altura)
-  return imc
+  var imc = peso / (altura * altura);
+  return imc;
 }
 
-console.log(imc(64,1.7))
+console.log(imc(64, 1.7));
 
 function corFavorita(cor) {
-  if(cor === "azul") {
-    return "Eu adoro o céu"
-  } else if( cor === 'verde'){
-    return "Eu gosto da natureza"
-  }else{
-    return "Eu não gosto de cores"
+  if (cor === "azul") {
+    return "Eu adoro o céu";
+  } else if (cor === "verde") {
+    return "Eu gosto da natureza";
+  } else {
+    return "Eu não gosto de cores";
   }
 }
 
-
-
-
-addEventListener('click', function() {
-  console.log('Clicou');
+addEventListener("click", function () {
+  console.log("Oi");
 });
 
 // A função possui dois argumentos
 // Primeiro é a string 'click' que significa quando o usuário clica em algo
 // Segundo é uma função anônima que é executada somente quando o primeiro argumento 'click' ocorre
-
