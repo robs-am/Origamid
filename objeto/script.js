5385f893d8ab2b36ab8077931b7e308b9955aa01
Roberta Amaro <roberta.amaro89@gmail.com>
2023-01-21 20:59:58 -0300

    definição de proriedades do objeto

--- objeto/script.js
+++ objeto/script.js
@@ -2,3 +2,4 @@ var pessoa = {
   nome: "Luiz",
   idade: 65,
 };
+//pessoa é o objeto e o que esta contido dentro das chaves são as propriedades. As propiedades são compostas por nome (chave) e valor
