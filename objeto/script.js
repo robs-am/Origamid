807c8d07f9735a06ac33c3ccc02ca55a729dfcc8
Roberta Amaro <roberta.amaro89@gmail.com>
2023-01-22 18:32:32 -0300

    add metodos

--- objeto/script.js
+++ objeto/script.js
@@ -1,5 +1,26 @@
 var pessoa = {
-  nome: "Luiz",
+  nome: "Luiz", //nome = chave, Luiz = valor
   idade: 65,
 };
 //pessoa é o objeto e o que esta contido dentro das chaves são as propriedades. As propiedades são compostas por nome (chave) e valor
+
+//metodos
+
+//É uma propriedade que possui uma função no local do seu valor.
+
+var quadrado = {
+  lados: 4, //propriedade
+  area: function (lado) {
+    //método, pois o valor dela é a função da area do quadrado
+
+    return lado * lado;
+  },
+  perimetro: function (lado) {
+    console.log(quadrado);
+    return lado * lado; //a paalavra reservada this em js, se refere no caso, ao objeto em si (lados), para nao ter que repetir a variavel lados
+  },
+};
+
+quadrado.lados; // 4
+console.log(quadrado.area(5)); // 25
+quadrado.perimetro(5); // 20
