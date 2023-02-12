e03ad783b0d54f3774ee7e16471bb372405acdd4
Roberta Amaro <roberta.amaro89@gmail.com>
2023-02-12 18:48:05 -0300

    add teste

--- objeto/script.js
+++ objeto/script.js
@@ -2,6 +2,8 @@ var pessoa = {
   nome: "Luiz", //nome = chave, Luiz = valor
   idade: 65,
 };
+
+pessoa.nome
 //objeto = var pessoa
 //propriedade = chave(ex.: nome) + valor (ex.: "Luiz)"
 // propriedade é tudo que esta contido dentro das chaves
@@ -36,16 +38,61 @@ quadrado.perimetro(5); // 20
 /* 
 ex.: console.log -> sempre que tiver um ponto, o que está antes dele é um objeto. E Log é um método, pois está executando uma função */
 
+var height = 120;
 var menu = {
   width: 800,
   height: 50,
   metadeHeight() {
-    return this.height / 2;
+    return this.height / 2; //o this vai se referir ao height do objeto menu. se retirarmos o this ele se referira ao height do escopo global, no caso 120
+
   }
+
+
 }
 
-menu.height
+menu.metadeHeight(); // 25
+// sem o this, seria 60
+
+
+
 
 var backgroundColor = menu.backgroundColor; //transforma a propriedade backGroudnColor em uma variável a parte
 
 //pode-se criar variáveis e acessar valores de outra variavel usando
+
+
+//EXERCÍCIO//
+
+// Crie um objeto com os seus dados pessoais
+var eu = {
+  nome: "Roberta",
+  sobrenome: "Amaro",
+  idade: "33",
+  cidade: "Rio de Janeiro",
+  nomeCompleto() {
+    return this.nome + this.cidade
+  }
+}
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+
+// Modifique o valor da propriedade preco para 3000
+
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 800;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+ var cachorro ={
+  raça: 'labrador', 'maria',
+  cor: '#000',
+  idade:10,
+  lateQuandoHomemPssa: true
+ }
