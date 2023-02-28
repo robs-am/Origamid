815d154fe9f4fecfd57c8465e00d16febdc74555
Roberta Amaro <roberta.amaro89@gmail.com>
2023-02-27 23:29:21 -0300

    add exercicios arrays

--- arrays2/script.js
+++ arrays2/script.js
@@ -14,8 +14,28 @@ for (var i = 0; i < videoGames.length; i++) {
 //i = index de cada elemento do array
 
 //forEach
-var frutas = ["pera", "mamão", "maçã", "abacaxi"];
+/* var frutas = ["pera", "mamão", "maçã", "abacaxi"];
 frutas.forEach(function (fruta, index, array) {
-  frutas.push("");
   console.log(fruta);
-});
+
+  frutas.push("oleo");
+}); */
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+
+var brasilCampeao = [1959, 1962, 1970, 1994, 2002];
+console.log(brasilCampeao);
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+
+for (let i = 0; i < brasilCampeao.length; i++) {
+  console.log(`O Brasil ganhou a copa de ${brasilCampeao[i]}`);
+}
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
