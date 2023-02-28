12bf2237ed0ca4c4fa49290f039ead87eac04966
Roberta Amaro <roberta.amaro89@gmail.com>
2023-02-28 10:00:19 -0300

    finaliza exercicio arrays

--- arrays2/script.js
+++ arrays2/script.js
@@ -24,18 +24,29 @@ frutas.forEach(function (fruta, index, array) {
 // Crie uma array com os anos que o Brasil ganhou a copa
 // 1959, 1962, 1970, 1994, 2002
 
-var brasilCampeao = [1959, 1962, 1970, 1994, 2002];
-console.log(brasilCampeao);
+/* var brasilCampeao = [1959, 1962, 1970, 1994, 2002];
+console.log(brasilCampeao); */
 
 // Interaja com a array utilizando um loop, para mostrar
 // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
 
-for (let i = 0; i < brasilCampeao.length; i++) {
+/* for (let i = 0; i < brasilCampeao.length; i++) {
   console.log(`O Brasil ganhou a copa de ${brasilCampeao[i]}`);
-}
+} */
 
 // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
 var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
 
+for (var fruta = 0; fruta < frutas.length; fruta++) {
+  if (frutas[fruta] === "Pera") {
+    break;
+  }
+  console.log(frutas[fruta]);
+}
+
 // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
+
+var ultimaFruta = frutas[4];
+
+var frutasPop = frutas.pop(); //fazendo dessa forma, ele retornará o item Melancia, porém o deleterá do array frutas
