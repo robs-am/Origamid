6ae6bca94b562fc3a5c4fee1318b12afe3faf119
Roberta Amaro <roberta.amaro89@gmail.com>
2023-03-09 22:49:09 -0300

    add classes e atributos

---
+++ classes-e-atributos/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+
+console.log(menu.classList); //mostra as classes
+
+menu.classList.add("ativo", "teste"); //adicona mais elementos
+menu.classList.remove("teste"); //adicona mais elementos
+menu.classList.toggle("teste"); //adiciona ou remove classes
