fe139ad150aa6d44d39cce0f7d39a4e2cb0b1adf
Roberta Amaro <roberta.amaro@ed3.com.br>
2023-03-12 18:03:06 -0300

    dimensoes e atributos exercicios

--- dimensoes-e-atributos/script.js
+++ dimensoes-e-atributos/script.js
@@ -1,40 +1,40 @@
-const menu = document.querySelector(".menu");
-/* 
-console.log(menu.classList); //mostra as classes
+//ECERCICIOS
 
-menu.classList.add("ativo", "teste"); //adicona mais elementos
-menu.classList.remove("teste"); //adicona mais elementos
-menu.classList.toggle("teste"); //adiciona ou remove classes
- */
-//Exercicios
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
 
-// Adicione a classe ativo a todos os itens do menu
-const itensMenu = document.querySelectorAll(".menu a");
+// Retorne a soma da largura de todas as imagens
 
-/* itensMenu.forEach((item) => {
-  item.classList.add("ativo");
-}); */
+/* function somaValores() {
+  const imagens = document.querySelectorAll("img"); //pega todas as imagens
 
-// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+  imagens.forEach((imagem) => {
+    //pega o tamanho de cada imagem
+    console.log(imagem.offsetWidth);
+  });
+}
 
-/* itensMenu.forEach((item) => {
-  item.classList.remove("ativo");
+window.onload = function () {
+  //evento que dispara assim que o site é carregado, no caso aqui vai disparar a soma de valores criado na função somaValores declarada acima
+  somaValores();
+}; */
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+
+const link = document.querySelectorAll("a");
+//console.log(links);
+
+const linksSize = link.matchMedia("(max-width: 48px)");
+
+link.forEach((link) => {
+  if (linksSize.matches >= 720) {
+    console.log("Possue o mínimo recomendado");
+  } else {
+    menu.classList.add(".menu-mobile");
+  }
 });
 
-itensMenu[0].classList.add("ativo"); */ //por itensMenu ser uma array, podemos acessar o primeiro item atrasves do index
-
-// Verifique se as imagens possuem o atributo alt
-
-/* const imagens = document.querySelectorAll("img");
-
-imagens.forEach((item) => {
-  const altAtributo = item.hasAttribute("alt");
-  console.log(item, altAtributo); //apenas com altAtributo verificará se é true or false,
-  //passando o item, ele verificará dentro de cada item também
-});
- */
-// Modifique o href do link externo no menu
-const linkExterno = document.querySelectorAll("a");
-console.log(linkExterno);
-
-linkExterno[3].setAttribute("href", "#");
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
