747c74b9b4b356ef00c8df0dc043d0ebe9d4a5f7
Roberta Amaro <roberta.amaro89@gmail.com>
2023-03-14 20:29:02 -0300

    events

--- eventos/script.js
+++ eventos/script.js
@@ -1,40 +1,24 @@
-//ECERCICIOS
+/* const imagem = document.querySelector("img"); */
 
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
+/* imagem.addEventListener("click", () => {
+  console.log("clicou");
+}); */
 
-// Retorne a soma da largura de todas as imagens
+/* function callback() {
+  console.log();
 
-/* function somaValores() {
-  const imagens = document.querySelectorAll("img"); //pega todas as imagens
+} */
 
-  imagens.forEach((imagem) => {
-    //pega o tamanho de cada imagem
-    console.log(imagem.offsetWidth);
-  });
+//imagem.addEventListener("click", callback()); //Se chamar a função com () será undefined
+
+//É uma boa pratica separar a função do eventListener a qual ela será chamada
+//a fim de ser mais facil depois para debugar o código
+
+const animaisLista = document.querySelector(".animais-lista");
+
+function callbackLista(event) {
+  console.log(event.target); //pega exatamente onde ocorreu o click
+  console.log(event.currentTarget); //pega toda a selção do querySelector
 }
 
-window.onload = function () {
-  //evento que dispara assim que o site é carregado, no caso aqui vai disparar a soma de valores criado na função somaValores declarada acima
-  somaValores();
-}; */
-
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-
-const link = document.querySelectorAll("a");
-//console.log(links);
-
-const linksSize = link.matchMedia("(max-width: 48px)");
-
-link.forEach((link) => {
-  if (linksSize.matches >= 720) {
-    console.log("Possue o mínimo recomendado");
-  } else {
-    menu.classList.add(".menu-mobile");
-  }
-});
-
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
+animaisLista.addEventListener("click", callbackLista);
