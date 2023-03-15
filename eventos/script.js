5c845bb1f955ac8519c60596751186d613c6e5cb
Roberta Amaro <roberta.amaro89@gmail.com>
2023-03-14 21:10:17 -0300

    add this

--- eventos/script.js
+++ eventos/script.js
@@ -23,13 +23,22 @@
 
 animaisLista.addEventListener("click", callbackLista); */
 
-const linkExterno = document.querySelector('a[href^="http"]');
+/* const linkExterno = document.querySelector('a[href^="http"]');
 
 function linkExternoHandle(event) {
   event.preventDefault();
   console.log(event);
 }
 
-linkExterno.addEventListener("click", linkExternoHandle);
+linkExterno.addEventListener("click", linkExternoHandle); */
 
 //usado para prevenir o padrão esperado, no caso como esta selecionado o href, o clique não dara abrir uma pagina externa
+
+const linkExterno = document.querySelector('a[href^="http"]');
+
+function linkExternoHandle(event) {
+  event.preventDefault();
+  console.log(this.getAttribute("href")); //this retorna o elemento que foi selecionado, no caso de eventos funcionara como o event.currentTarget
+}
+
+linkExterno.addEventListener("click", linkExternoHandle);
