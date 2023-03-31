e2770af562f34ef1a683ccb30b86e820a3c66db7
Roberta Amaro <roberta.amaro@ed3.com.br>
2023-03-30 21:04:43 -0300

    eventos

--- eventos/script.js
+++ eventos/script.js
@@ -7,7 +7,7 @@
 /* function callback() {
   console.log();
 
-} */
+} 
 
 //imagem.addEventListener("click", callback()); //Se chamar a função com () será undefined
 
@@ -30,7 +30,7 @@ function linkExternoHandle(event) {
   console.log(event);
 }
 
-linkExterno.addEventListener("click", linkExternoHandle); */
+linkExterno.addEventListener("click", linkExternoHandle); 
 
 //usado para prevenir o padrão esperado, no caso como esta selecionado o href, o clique não dara abrir uma pagina externa
 
@@ -41,15 +41,10 @@ function linkExternoHandle(event) {
   console.log(this.getAttribute("href")); //this retorna o elemento que foi selecionado, no caso de eventos funcionara como o event.currentTarget
 }
 
-linkExterno.addEventListener("click", linkExternoHandle); */
-/* 
-function handleKeyboard(event) {
-  if (event.key === "a") document.body.classList.toggle("azul");
-  else if (event.key === "v") document.body.classList.toggle("vermelho");
-}
+linkExterno.addEventListener("click", linkExternoHandle); 
 
-window.addEventListener("keydown", handleKeyboard);
-*/
+
+
 
 /* const imgs = document.querySelectorAll("img"); //pega todos os seletores de imagem
 
@@ -85,21 +80,98 @@ imgs.forEach((img) => {
 // demais itens caso eles possuam a mesma. Previna
 // o comportamento padrão desses
 
-const linksInternos = document.querySelectorAll('a[href^="#"]'); //seleciona links internos
+//const linksInternos = document.querySelectorAll('a[href^="#"]'); //seleciona links internos
 
-function handleLink(event) {
+/* function handleLink(event) {
   event.preventDefault(); //previne o comportamento usual do link
+  linksInternos.forEach((link) => {
+    link.remove.classList("ativo");
+    //loop que remove a classe de todos os links
+  });
 
-  event.currentTarget.classList.toggle("ativo"); //adiciona ou remove a classe
+  event.currentTarget.classList.add("ativo"); //adiciona a classe somente ao link clicado
 }
 
 linksInternos.forEach((link) => {
-  //adiciona o evento clique para cada link interno no diste
+  //adiciona o evento clique para cada link interno do site
   link.addEventListener("click", handleLink);
+});
+ */
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+
+/* const selecionaBody = document.querySelectorAll("body *");
+console.log(selecionaBody);
+
+function handleBody(event) {
+  event.preventDefault();
+  this.getAttribute();
+}
+
+selecionaBody.addEventListener("click", handleBody); */
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+
+/* function handleKeyboard(event) {
+  if (event.key === "a") document.body.classList.toggle("azul");
+  else if (event.key === "v") document.body.classList.toggle("vermelho");
+}
+
+window.addEventListener("keydown", handleKeyboard); */
+
+//adiciona fullscreen
+/* function handleKeyboard(event) {
+  if (event.key === "f") {
+    document.body.classList.toggle("fullscreen");
+  }
+}
+
+window.addEventListener("keydown", handleKeyboard); */
+
+/* const imgs = document.querySelectorAll("img");
+
+function handleImage(event) {
+  console.log(event.currentTarget.getAttribute("src"));
+}
+
+imgs.forEach((img) => {
+  img.addEventListener("click", handleImage);
+}); */
+
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function handleClick(event) {
+  event.preventDefault();
+
+  linksInternos.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  event.currentTarget.classList.add("ativo");
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener("click", handleClick);
 });
 
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
+
+const elementosPagina = document.querySelectorAll("body *");
+
+function handleBody(event) {
+  event.preventDefault();
+  this.getAttribute();
+}
+
+elementosPagina.addEventListener("click", handleBody);
 
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
