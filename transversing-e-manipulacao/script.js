9aa151b3c9febecb631d6fb40b4f1980f24ef83e
Roberta Amaro <roberta.amaro89@gmail.com>
2023-04-04 11:46:26 -0300

    create element

--- transversing-e-manipulacao/script.js
+++ transversing-e-manipulacao/script.js
@@ -1,9 +1,38 @@
-const h1 = document.querySelector("h1");
+/* const h1 = document.querySelector("h1");
 
 h1.innerHTML = "<h1>Outro título</h1>"; //mudará o título mantendo as propriedades da fonte (tamanho, escalada, tipo)
 
-console.log(h1.innerHTML);
+console.log(h1.innerHTML); */
 
 /* h1.outerHTML = "<h1>Outro título</h1>"; */
 
 /* console.log(h1.outerHTML); */ //muda o título substituindo todas as propriedades
+
+//CREATE ELEMENT
+// const mapa = document.querySelector(".mapa");
+// const novoh1 = document.createElement("h1"); //método do DOM que cria um novo elemento h1. Não aparecerá no dom ainda, apenas no console
+
+// novoh1.innerText = "Texto Create Element"; //propriedade que adiciona novo texto
+
+// novoh1.classList.add("titulo"); //método que adiciona nova classe ao elemnto criado
+
+// console.log(novoh1);
+
+// mapa.appendChild(novoh1); //após o mapa, o novoh1 será adicionado ao dom após seu primeiro filho (imagem)
+
+//exercícios
+
+// Duplique o menu e adicione ele em copy
+const menu = document.querySelector(".menu");
+
+const cloneMenu = menu.cloneNode(true); //clonará o menu
+console.log(menu);
+const faq = document.querySelector(".faq");
+
+faq.appendChild(cloneMenu); //colocará o menu após o primeiro filho do faq
+
+// Selecione o primeiro DT da dl de Faq
+
+// Selecione o DD referente ao primeiro DT
+
+// Substitua o conteúdo html de .faq pelo de .animais
