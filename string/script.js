4ca02f280f233a328c4921423ad2b1daab6b3af0
Roberta <roberta.amaro@ed3.com.br>
2023-11-19 12:43:44 -0300

    numer e math

--- string/script.js
+++ string/script.js
@@ -1,7 +1,7 @@
 // Utilizando o foreach na array abaixo,
 // some os valores de Taxa e os valores de Recebimento
 
-const transacoes = [
+/* const transacoes = [
   {
     descricao: "Taxa do Pão",
     valor: "R$ 39",
@@ -23,13 +23,27 @@ const transacoes = [
     valor: "R$ 49",
   },
 ];
+let taxaTotal = 0;
+let recebimentoTotal = 0;
 
 transacoes.forEach((item) => {
-  console.log(item);
+  const numeroLimpo = +item.valor.replace("R$ ", ""); //colocar o + na frente transforma o número que esta com valor de string em number
+
+  if (item.descricao.slice(0, 4) === "Taxa") {
+    taxaTotal = taxaTotal + numeroLimpo;
+  } else {
+    recebimentoTotal += numeroLimpo;
+  }
 });
+console.log(taxaTotal);
+console.log(recebimentoTotal); */
 
 // Retorne uma array com a lista abaixo
 const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
+
+let arrayTransportes = transportes.split(";");
+
+console.log(arrayTransportes);
 
 // Substitua todos os span's por a's
 const html = `<ul>
@@ -38,14 +52,22 @@ const html = `<ul>
                 <li><span>Contato</span></li>
               </ul>`;
 
+const novoHtml = html.split("span").join("a");
+console.log(novoHtml);
+
 // Retorne o último caracter da frase
 const frase = "Melhor do ano!";
+console.log(frase.slice(-1));
 
 // Retorne o total de taxas
-/* const transacoes = [
+const transacoes2 = [
   "Taxa do Banco",
   "   TAXA DO PÃO",
   "  taxa do mercado",
   "depósito Bancário",
   "TARIFA especial",
-]; */
+];
+
+transacoes2.forEach((item) => {
+  console.log(item);
+});
